d5dbd715471e57de09b100a6e6b185f06a97163c
juakin <juakinb3rnardo@gmail.com>
2025-04-30 19:37:39 -0400

    header y footer, el slider falla

--- FerremaxUnlimited/app/urls.py
+++ FerremaxUnlimited/app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import home,contactos,productos,servicios
 
 urlpatterns = [
-    path('', home, name="Home"),
-    path('Contacto/',contactos, name='contacto'),
-    path('Productos/',productos, name='productos'),
-    path('Servicios/',servicios, name='servicios'),
+    path('', home, name="home"),
+    path('contacto/',contactos, name="contactos"),
+    path('productos/',productos, name="productos"),
+    path('servicios/',servicios, name="servicios"),
 ]
 
 
--- FerremaxUnlimited/app/views.py
+++ FerremaxUnlimited/app/views.py
@@ -6,10 +6,10 @@ def home(request):
     return render(request, 'app/Home.html')
 
 def contactos(request):
-    return render(request, 'app/Contactos,html')
+    return render(request, 'app/Contactos.html')
 
 def productos(request):
-    return render(request, 'app/Productos,html')
+    return render(request, 'app/Productos.html')
 
 def servicios(request):
-    return render(request, 'app/Servicios,html')
+    return render(request, 'app/Servicios.html')
